021bc5a0665d35a33226eb079107f1ad6162a928
Peter Richmond <3448039+burdockcascade@users.noreply.github.com>
2025-08-17 21:55:01 +0100

    feat: implement basic window functionality with winit and logging (#1)

---
+++ examples/hello_window.rs
@@ -0,0 +1,7 @@
+use rgfx::prelude::*;
+
+fn main() {
+    let window = Window::new(800, 600, "Hello Window");
+
+    window.run();
+}
---
+++ src/graphics/window.rs
@@ -0,0 +1,91 @@
+use log::{debug, error};
+use std::sync::Arc;
+use winit::application::ApplicationHandler;
+use winit::event::WindowEvent;
+use winit::event_loop::{ActiveEventLoop, EventLoop};
+use winit::keyboard::{KeyCode, PhysicalKey};
+
+pub struct Window {
+    winit_window: Option<Arc<winit::window::Window>>,
+    window_attributes: winit::window::WindowAttributes
+}
+
+impl Window {
+
+    pub fn new(width: u32, height: u32, title: &str) -> Self {
+        let window_attributes = winit::window::Window::default_attributes()
+            .with_title(title)
+            .with_inner_size(winit::dpi::PhysicalSize::new(width, height));
+
+        Self {
+            window_attributes,
+            winit_window: None
+        }
+    }
+
+    pub fn run(mut self) {
+        match EventLoop::new() {
+            Ok(event_loop) => {
+                if let Err(e) = event_loop.run_app(&mut self) {
+                    error!("Failed to run app: {}", e);
+                }
+            },
+            Err(e) => error!("Error creating event loop: {}", e)
+        }
+    }
+
+}
+
+impl ApplicationHandler for Window {
+
+    fn resumed(&mut self, event_loop: &ActiveEventLoop) {
+
+        let window = match event_loop.create_window(self.window_attributes.clone()) {
+            Ok(window) => Arc::new(window),
+            Err(e) => {
+                error!("Error creating window: {}", e);
+                return;
+            }
+        };
+
+        self.winit_window = Some(window.clone());
+
+    }
+
+    fn window_event(&mut self, event_loop: &ActiveEventLoop, _window_id: winit::window::WindowId, event: WindowEvent) {
+
+        match event {
+            WindowEvent::CloseRequested => {
+                event_loop.exit();
+            }
+            WindowEvent::KeyboardInput { event, .. } => {
+                match event.physical_key {
+                    PhysicalKey::Code(code) => {
+                        if code == KeyCode::Escape {
+                            event_loop.exit();
+                        }
+                    },
+                    _ => {
+                        debug!("Unhandled physical key: {:?}", event.physical_key);
+                    }
+                }
+            }
+            _ => {
+                debug!("Unhandled window event: {:?}", event);
+            }
+        }
+
+    }
+
+    fn about_to_wait(&mut self, event_loop: &ActiveEventLoop) {
+        match self.winit_window {
+            Some(ref window) => {
+                window.request_redraw();
+            }
+            None => {
+                error!("Window not yet initialized");
+                event_loop.exit();
+            }
+        }
+    }
+}
